5ace9cd99dd5d8a4d5b4c76352c558641a33b9dd
Milton Figueredo Miles <gratno@gmail.com>
2023-04-13 14:17:18 -0500

    SELECT DISTINCT

--- mysqlaluraConsultasLIKE.sql
+++ mysqlaluraConsultasLIKE.sql
@@ -2,3 +2,5 @@ SELECT * FROM tabla_de_productos WHERE SABOR LIKE '%manzana';
 
 SELECT * FROM tabla_de_productos WHERE SABOR LIKE '%manzana'
 AND ENVASE = 'Botella PET';
+
+SELECT * FROM tabla_de_clientes WHERE NOMBRE LIKE '%ez';
