e16ea3ef41f4a42d311110ccc8f87a6d30791be8
Milton Figueredo Miles <gratno@gmail.com>
2023-04-14 19:12:45 -0500

    Use HAVING

--- mysqlaluraConsultasLIMIT.sql
+++ mysqlaluraConsultasLIMIT.sql
@@ -1,1 +1,9 @@
-SELECT  * FROM tabla_de_productos LIMIT 4;
+SELECT  * FROM tabla_de_productos LIMIT 3,2;
+
+SELECT  * FROM tabla_de_productos LIMIT 20, 3;
+
+SELECT  * FROM tabla_de_productos LIMIT 5;
+
+SELECT  * FROM tabla_de_productos LIMIT 5, 4;
+
+SELECT  * FROM facturas LIMIT 5, 4;
