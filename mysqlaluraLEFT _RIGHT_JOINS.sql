8eb2313c1927810b73fe7dcffa89f044434e1f2b
Milton Figueredo Miles <gratno@gmail.com>
2023-04-17 20:55:04 -0500

    LEFT y RIGHT JOIN - MySQL

---
+++ mysqlaluraLEFT _RIGHT_JOINS.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(*) FROM tabla_de_clientes;
+
+SELECT A.DNI, A.NOMBRE, B.DNI 
+FROM tabla_de_clientes A 
+INNER JOIN facturas  B
+ON A.DNI = B.DNI;
+
+SELECT DISTINCT A.DNI, A.NOMBRE, B.DNI 
+FROM tabla_de_clientes A 
+INNER JOIN facturas  B
+ON A.DNI = B.DNI;
