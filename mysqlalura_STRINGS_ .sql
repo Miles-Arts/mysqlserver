359408d6333c1c25c90d45709a9b7d917446fe93
Milton Figueredo Miles <gratno@gmail.com>
2023-04-19 18:41:53 -0500

    Funtion STRINGS

---
+++ mysqlalura_STRINGS_ .sql
@@ -0,0 +1,15 @@
+SELECT LTRIM("   MySQL es fácil");
+
+SELECT RTRIM("   MySQL es fácil    ");
+
+SELECT TRIM("   MySQL es fácil   ");
+
+SELECT CONCAT("MySQL es fácil,",  " No lo crees?");
+
+SELECT UPPER("mysql es una base de datos interesante");
+
+SELECT LOWER("MYSQL ES UNA BASE DE DATOS INTERESANTE");
+
+SELECT SUBSTRING("mysql es una base de datos interesante", 14,4);
+
+SELECT CONCAT(NOMBRE, " ", DNI ) FROM tabla_de_clientes;
