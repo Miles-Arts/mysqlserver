4bfc3501b9afccb12bee77f7ea4169b57d5a0b91
Milton Figueredo Miles <gratno@gmail.com>
2023-04-19 21:03:28 -0500

    CONVIRTIENDO DATOS

--- mysqlalura_CONVIRTIENDO DATOS .sql
+++ mysqlalura_CONVIRTIENDO DATOS .sql
@@ -1,1 +1,39 @@
-SELECT CURRENT_TIMESTAMP()
+SELECT CURRENT_TIMESTAMP() AS RESULTADO;
+
+SELECT CONCAT("La fecha y la hora de hoy son:  " , 
+CURRENT_TIMESTAMP) AS RESULTADO;
+
+SELECT CONCAT("La fecha y el horario son:  " , 
+DATE_FORMAT(CURRENT_TIMESTAMP(), "%Y")) AS RESULTADO;
+
+SELECT CONCAT("La fecha y el horario son:  " , 
+DATE_FORMAT(CURRENT_TIMESTAMP(), "%y")) AS RESULTADO;
+
+SELECT CONCAT("La fecha y el horario son:  " , 
+DATE_FORMAT(CURRENT_TIMESTAMP(),  "Mes y Año, %m ,%y")) 
+AS RESULTADO;
+
+SELECT CONCAT("La fecha y el horario son:  " , 
+DATE_FORMAT(CURRENT_TIMESTAMP(),  "Mes %m / Año %y")) 
+AS RESULTADO;
+
+SELECT CONCAT("La fecha y el horario son:  " , 
+DATE_FORMAT(CURRENT_TIMESTAMP(),  " Day %d / Month %m / Year %y")) 
+AS RESULTADO;
+
+
+SELECT CONCAT("La fecha y el horario son:  " , 
+DATE_FORMAT(CURRENT_TIMESTAMP(),  "%W / Day %d / Month %m / Year %y")) 
+AS RESULTADO;
+
+SELECT CONCAT("La fecha y el horario son:  " , 
+DATE_FORMAT(CURRENT_TIMESTAMP(),  
+"%W / Day %d / Month %m / Year %y / A time  %T")) 
+AS RESULTADO;
+
+SELECT CONVERT(23.45, CHAR) AS RESULTADO;
+
+SELECT SUBSTRING(CONVERT(23.45, CHAR), 3,1 );
+
+
+
