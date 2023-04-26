d587ad4360dae6b5a6e7a9ec948851e9ab5f5f71
Milton Figueredo Miles <gratno@gmail.com>
2023-04-25 19:18:02 -0500

    Incluyendo DATOS en tb_productos

---
+++ mysqlDml INCLUYENDO DATOS .sql
@@ -0,0 +1,49 @@
+USE ventas_jugos;
+
+INSERT INTO tb_producto ( 
+CODIGO, 
+DESCRIPCION, 
+SABOR, 
+TAMANO, 
+ENVASE, 
+PRECIO_LISTA
+) 
+VALUES (
+'1040107',
+'Light',
+'Sandía',
+'350 ml',
+'Lata',
+4.56
+);
+
+SELECT * FROM tb_producto;
+
+
+INSERT INTO tb_producto ( 
+CODIGO, 
+DESCRIPCION, 
+SABOR, 
+TAMANO, 
+ENVASE, 
+PRECIO_LISTA
+) 
+VALUES (
+'1040107',
+'Light',
+'Sandía',
+'350 ml',
+'Lata',
+4.56
+);
+
+
+
+
+
+
+
+
+
+
+
