34251beb9be0518a5a295a137a97a11feca2a2ad
Milton Figueredo Miles <gratno@gmail.com>
2023-04-26 19:42:13 -0500

    MySQL UPDATE FROM

---
+++ mysqlDml UPDATE .sql
@@ -0,0 +1,39 @@
+USE ventas_jugos;
+
+SELECT * FROM tb_producto; 
+
+UPDATE tb_producto SET PRECIO_LISTA = 5
+WHERE CODIGO = '1000889';
+
+SELECT * FROM tb_producto; 
+
+UPDATE tb_producto SET DESCRIPCION = 'Sabor de la Montaña',
+TAMANO = '1 Litro',  ENVASE = 'Botella PET'
+WHERE CODIGO = '1000889';
+
+SELECT * FROM tb_producto; 
+
+
+SELECT * FROM tb_cliente;
+
+UPDATE tb_cliente SET VOLUMEN_COMPRA = VOLUMEN_COMPRA/10;
+
+SELECT * FROM tb_cliente;
+
+UPDATE tb_clientes SET 
+DIRECCION = 'Jorge Emilio 23',
+BARRIO = 'San Antonio',
+CIUDAD = 'Guadalajara',
+ESTADO = 'JC',
+CEP = '44700000'
+WHERE DNI = '5840119709'
+
+
+
+
+
+
+
+
+
+
