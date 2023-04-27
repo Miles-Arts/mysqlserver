ab4ce7ac4bfffd5824568a2c6d463f1f9eaedc3b
Milton Figueredo Miles <gratno@gmail.com>
2023-04-26 20:46:19 -0500

    MySQL COMMIT y ROLLBACK

---
+++ mysqlDml COMMIT and ROLLBACK .sql
@@ -0,0 +1,21 @@
+
+INSERT INTO `ventas_jugos`.`tb_vendedor`
+(`MATRICULA`,
+`NOMBRE`,
+`BARRIO`,
+`COMISION`,
+`FECHA_ADMISION`,
+`DE_VACACIONES`)
+VALUES
+('256',
+'Fernando Ruiz',
+'Oblatos',
+0.1,
+'2015-06-14',
+0);
+
+
+
+
+
+
