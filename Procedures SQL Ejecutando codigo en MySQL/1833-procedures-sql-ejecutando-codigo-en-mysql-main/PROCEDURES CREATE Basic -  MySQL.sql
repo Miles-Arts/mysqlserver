3f841b84c4ac181a20422b83adfd40a4e9d6fc8c
Milton Figueredo Miles <gratno@gmail.com>
2023-04-28 19:14:26 -0500

    DECLARANDO VARIABLES - MySQL

--- Procedures SQL Ejecutando codigo en MySQL/1833-procedures-sql-ejecutando-codigo-en-mysql-main/PROCEDURES CREATE Basic -  MySQL.sql
+++ Procedures SQL Ejecutando codigo en MySQL/1833-procedures-sql-ejecutando-codigo-en-mysql-main/PROCEDURES CREATE Basic -  MySQL.sql
@@ -1,6 +1,13 @@
 /*  Letras números y $ y _ 
 tamaño máximo 64 carácteres , 
 nombre único 
+las variables máximo 255 caracteres
+VARCHAR 
+INTEGER
+DECIMAL
+DATE
+TIMESTAMP
+
 CASE SENSITIVE*/
 
 
@@ -16,6 +23,7 @@ CALL concatenar;
 
 CALL concatenar_con_comentarios;
 
+CALL concatenar2;
 
 CALL concatenar2;
 
