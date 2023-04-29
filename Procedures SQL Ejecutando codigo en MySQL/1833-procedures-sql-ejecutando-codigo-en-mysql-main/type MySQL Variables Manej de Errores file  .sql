2e606c92d61a2ed668843b70e8283a29460c846e
Milton Figueredo Miles <gratno@gmail.com>
2023-04-28 22:29:20 -0500

    Atribución de valor usando SELECT- DENTRO de STORED Procedure - MySQL

---
+++ Procedures SQL Ejecutando codigo en MySQL/1833-procedures-sql-ejecutando-codigo-en-mysql-main/type MySQL Variables Manej de Errores file  .sql
@@ -0,0 +1,11 @@
+SELECT * FROM tabla_de_productos;
+
+SELECT SABOR FROM tabla_de_productos
+WHERE CODIGO_DEL_PRODUCTO = "1078680";
+
+
+SELECT CODIGO_DEL_PRODUCTO, SABOR FROM tabla_de_productos;
+
+CALL mostrar_sabor('1101035'); 
+
+
