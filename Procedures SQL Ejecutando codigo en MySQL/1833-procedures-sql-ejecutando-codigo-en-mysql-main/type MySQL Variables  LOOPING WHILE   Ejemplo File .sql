c517e2d065460d8c189ff399b4df765ddcbc35aa
Milton Figueredo Miles <gratno@gmail.com>
2023-05-01 18:56:47 -0500

    Looping WHILE MySQL

--- Procedures SQL Ejecutando codigo en MySQL/1833-procedures-sql-ejecutando-codigo-en-mysql-main/type MySQL Variables  LOOPING WHILE   Ejemplo File .sql
+++ Procedures SQL Ejecutando codigo en MySQL/1833-procedures-sql-ejecutando-codigo-en-mysql-main/type MySQL Variables  LOOPING WHILE   Ejemplo File .sql
@@ -1,10 +1,14 @@
+USE jugos_ventas;
 
+CREATE TABLE tb_looping (ID INT);
 
+SELECT * FROM tb_looping;
 
+/* VALOR INICAL 1 Y VALOR FINAL 10 o MIL */
+CALL looping(1,1000);
 
-
-
-
+CALL looping(1,10);
+CALL looping(1,10000);
 
 
 
