711bb0e0520ff31549345287d94bcc3c375d56c8
Milton Figueredo Miles <gratno@gmail.com>
2023-05-01 19:17:46 -0500

    FUNCION - CURSOR MySQL

---
+++ Procedures SQL Ejecutando codigo en MySQL/1833-procedures-sql-ejecutando-codigo-en-mysql-main/type MySQL Variables  CURSOR - FILE  .sql
@@ -0,0 +1,19 @@
+/* LÍNEA A LÍNEA EN UN ORDEN DETERMINADO*/
+/*
+DECLARACIÓN
+ABERTURA
+RECORRIDO
+CIERRE LIMPIAR
+*/
+
+CALL cursor_1;
+
+SELECT NOMBRE FROM tabla_de_clientes LIMIT 4;
+
+
+
+
+
+
+
+
