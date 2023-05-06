090a9dfb7ace68998ad0adfe8cd325bd084faa6a
Milton Figueredo Miles <gratno@gmail.com>
2023-05-05 19:48:07 -0500

    USED LIMIT - number RAND -DNI clientes  - MySQL

--- EMPRESA MySQL/Funcion MAX and TABLE -.sql
+++ EMPRESA MySQL/Funcion MAX and TABLE -.sql
@@ -1,12 +1,21 @@
 /*Para obtener el valor máximo de una tabla*/
+SELECT COUNT(*) FROM clientes;
 
-SELECT COUNT
+/* para llamar toda mi  BD - Base de Datos*/
+USE empresa;
 
+/*Para llamar una Tabla de la BD*/
+SELECT * FROM clientes;
 
+/*función LIMIT sólopara llamar los 5 primeras líneas de la BD*/
+SELECT * FROM clientes LIMIT 5;
 
+/*Para selecionar el cliente siguiente de una línea especifica */
+SELECT * FROM clientes LIMIT 5, 1;
 
-
-
+/*valor nulo al llamar un parametro Vacío */
+SELECT * FROM clientes LIMIT 15, 1;
+SELECT * FROM clientes LIMIT 16, 1;
 
 
 
