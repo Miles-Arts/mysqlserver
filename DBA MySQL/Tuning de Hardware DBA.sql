82bd9c8eff8eebe222003f24853bfb08d3618afb
Milton Figueredo Miles <gratno@gmail.com>
2023-05-06 21:29:10 -0500

    Tuning de HARDWARE para MySQL

---
+++ DBA MySQL/Tuning de Hardware DBA.sql
@@ -0,0 +1,33 @@
+/*
+formas de Configurar el 
+Ambiente MYSQL hacer Tuning -Mejorar la DB
+
+* Esquemas indices, variables internar MySQL
+	(mysqld)  y Hardware/Sistema Operativo
+    
+    - Hardware 64 bits - consume bastante memoria
+    - Configuración RAM - no pasar del 50% para procesos MySQL
+    - NOTA:
+			En un ambiente REAL 8 gigas de RAM es poco
+            tipo de lectura de disco HDD/SSD -Solidos + rápidos
+            dicos SCSI, SATA -SAS -- estado mecánico + lentos-
+			Controladores RAID(0, 1, 5 y 10)
+            los RAID 1 y 10 los MáS SEGUROS
+    
+ 
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
