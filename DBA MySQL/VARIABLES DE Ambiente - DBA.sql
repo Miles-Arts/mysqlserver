e1cc1ee7d51c253de5d3c88809626002657f4b06
Milton Figueredo Miles <gratno@gmail.com>
2023-05-08 19:23:42 -0500

    Variables de ambiente

--- DBA MySQL/VARIABLES DE Ambiente - DBA.sql
+++ DBA MySQL/VARIABLES DE Ambiente - DBA.sql
@@ -1,12 +1,26 @@
 /*Variables de Ambiente*/
 
+/*
+Variables que se declaran fuera del programa predefinidas 
+antes de la inicialización
+
+Más de 250 variables de MySQL
 
 
+SHOW STATUS
+-- Globales
+-- Session
 
 
+la ruta es   C:\ProgramData\MySQL
 
+*/
 
+SHOW STATUS;
 
+SHOW GLOBAL STATUS LIKE 'Created_tmp_disk_tables';
+
+SHOW GLOBAL STATUS LIKE 'CReate_tmp_tables';
 
 
 
