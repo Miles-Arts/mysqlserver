0c2f670de0b75b9f0fc16f3aa81e48721d64d2d6
Milton Figueredo Miles <gratno@gmail.com>
2023-05-09 20:39:19 -0500

    Back UP - mysqldump

--- DBA MySQL/Back up - mysqldump   - DBA.sql
+++ DBA MySQL/Back up - mysqldump   - DBA.sql
@@ -1,3 +1,41 @@
+/*BACK UP
+Copia de seguridad DB
+se hace periodicamente 
+
+cuidado al hacer los  delete - DROP DATABSe
+
+--BACK UP Logico--
+comando rutunas
+tablas, los exporta a un script sql -
+permite manipulación externa
+--es lenta por los comandos-
+
+
+BACK UP Físico
+Contiene toda la información de la DB en archivos binarios
+sin comandos.
+-file compacto--
+--es rápida- y menos fleixoble
+
+MYSQL-DUMP
+mysql[options]
+
+usuario-contraseña-servidos
+all-databases.. cuando el back está completo
+><nombre_archivo> muestra el archivo de salida
+Stored Procedures usar --routines y events
+
+
+
+
+
+
+
+
+
+*/
+
+
 
 
 
