307680c6bacf49730aefb98b951530bb7d1904b5
Milton Figueredo Miles <gratno@gmail.com>
2023-05-11 19:56:26 -0500

    MySQL DBA - B-TREE nodos

--- DBA MySQL/DBA II/INDICES  FIle  DBA II   MySQL.sql
+++ DBA MySQL/DBA II/INDICES  FIle  DBA II   MySQL.sql
@@ -5,15 +5,17 @@ Facilita ls busqueda dentro de la tabla (Diccionario)
 Recorre toda la tabla hasta encontrar el registro
 
 
-INDICEs en MyISQM
+INDICEs en MyISAM
 Modifica la tabla Original = los indices se actualizan
 Muchos indices, más tiempo de ejecución
+
 
 
 ÍDICES  InnoDB
 Utiliza la Clave Primaria 
 - En la PK la info ya estaría organizada si la PK es el indice
-
+- en el ID no puee haber 2 campos iguales - ejemplo placa de carros 
+InnoDB Pk es muy rápida
 
 
 
