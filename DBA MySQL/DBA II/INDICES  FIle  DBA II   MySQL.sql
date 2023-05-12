863416fe5944cd5ff259d589a11ccd60f324c9e1
Milton Figueredo Miles <gratno@gmail.com>
2023-05-11 19:36:18 -0500

    MySQL DBA - Indices con MyISAM y InnoDB

---
+++ DBA MySQL/DBA II/INDICES  FIle  DBA II   MySQL.sql
@@ -0,0 +1,39 @@
+/*
+
+Facilita ls busqueda dentro de la tabla (Diccionario)
+
+Recorre toda la tabla hasta encontrar el registro
+
+
+INDICEs en MyISQM
+Modifica la tabla Original = los indices se actualizan
+Muchos indices, más tiempo de ejecución
+
+
+ÍDICES  InnoDB
+Utiliza la Clave Primaria 
+- En la PK la info ya estaría organizada si la PK es el indice
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
